706728cde4110caf080beaf3a27770759b3faa28
Martin Kühl <martin.kuehl@gmail.com>
2010-03-18 10:55:16 +0100

    Use cocoa selection color when available.

--- color-theme-espresso.el
+++ color-theme-espresso.el
@@ -18,7 +18,7 @@
 
 Port of the default theme for Espresso on Mac OS X."
   (interactive)
-  (let ((selection-color "#C9D0D9")
+  (let ((selection-color (if (featurep 'ns) "ns_selection_color" "#C9D0D9"))
         (highlight-color "#EEE00A")
         (secondary-color "#FBE9AD")
         (passive-color "#AAAAAA")
